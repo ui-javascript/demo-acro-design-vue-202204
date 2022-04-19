6f7531ae74ad3ab4a73aabf8fe201bd395a452a7
骆金参 <luo960412@gmail.com>
2022-04-19 11:20:45 +0000

    adjust

---
+++ _arco/split.vue
@@ -0,0 +1,30 @@
+<template>
+  <Split :style="{
+        height: '200px',
+        width: '100%',
+        minWidth: '500px',
+        border: '1px solid var(--color-border)'
+      }"
+      v-model:size="size"
+      min="80px"
+    >
+      <template #first>
+        <TypographyParagraph>Right</TypographyParagraph>
+      </template>
+
+      <template #second>
+        <TypographyParagraph>Left</TypographyParagraph>
+      </template>
+    </Split>
+</template>
+
+<script setup>
+const {ref} from "vue"
+const size = ref(0.5)
+</script>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
